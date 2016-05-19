1f26df49252fab983c25405608992f31bf12edd1
curtjenk <curtjenk@comcast.net>
2016-05-19 09:16:20 -0400

    ignore db_connect.php

--- assets/includes/db_connect.php
+++ assets/includes/db_connect.php
@@ -6,39 +6,6 @@
   DB::$dbName= 'mantaray';
   DB::$user = 'x';
   DB::$password = 'x';
-
-//Determine if we are to test if we can successfully conntect to the DB
-   // if (isset($_GET['test']) && $_GET['test'] == 'true') {
-   //     $dbStatus = openDBConnection();
-   //     echo json_encode($dbStatus);
-   //     exit;
-   // }
-       
-  //  $dbStatus = openDBConnection();
-
-  
-  // function openDBConnection() {
-  //     $rtn = [];
-  //     $rtn['status'] = 'success';
-
-  //     $link = @mysql_connect('127.0.0.1', 'x', 'x');
-
-  //     if(!$link){
-  //       $rtn['status'] = 'fail';
-  //       $rtn['reason'] = 'Not Connected';
-  //       return $rtn;
-  //     } 
-      
-  //     $db_selected = @mysql_select_db('bookmarks', $link);
-  //     if(!$db_selected){
-  //       $rtn['status'] = "fail";
-  //       $rtn['reason'] = "Cannot use database bookmarks";
-        
-  //    } 
-    
-  //    return $rtn;
-  // }	
-
 ?>
 
 
